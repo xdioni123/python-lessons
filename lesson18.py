165c26932dc3e6b3e034a953ed40437196e38242
xdioni123 <dion.ibrahimi2008@gmail.com>
2025-09-18 20:36:42 +0200

    updated with charts

--- lesson18.py
+++ lesson18.py
@@ -26,3 +26,35 @@ col1.metric("Total books",totalbooks)
 col2.metric("Name",unique_name)
 col3.metric("Raiting",avarage_rating)
 col4.metric("Price",avarage_price)
+
+st.subheader("Stats")
+st.write(books_df.head())
+
+col1 ,col2 = st.columns(2)
+
+with col1:
+    st.subheader("Top 10 most sold books")
+    st_titles=books_df["Name"].value_counts().head(10)
+    st.bar_chart(st_titles)
+
+with col2:
+    st.subheader("Top 10 viewd authors")
+    st_authors=books_df["Author"].value_counts().head(10)
+    st.bar_chart(st_authors)
+
+st.subheader("Gemre Distribution")
+fig = px.pie(books_df, names="Genre",title='Most liked Genre 2009-2022', color="Genre",
+color_discrete_sequence=px.colors.sequential.Plasma)
+st.plotly_chart(fig)
+
+st.subheader("Top 15 Authors")
+top_authors = books_df['Author'].value_counts().head(15).reset_index()
+top_authors.columns=['Author','Count']
+
+figg = px.bar(top_authors, x='Count', y='Author',orientation='h',
+              title='Top 15 authors',
+              labels={'Count': 'Counts of Books Published','Author':'Author'},
+              color='Count', color_continuous_scale=px.colors.sequential.Plasma
+              )
+
+st.plotly_chart(figg)
